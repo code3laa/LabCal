5f39ac93e7131b216a5f57a65a7146430d6c8b77
Alaa Abdou <100050970+code3laa@users.noreply.github.com>
2023-10-20 21:58:55 +0300

    Add files via upload

--- labcal.js
+++ labcal.js
@@ -50,8 +50,44 @@ function nbrs() {
   let corrwbs = WbcsMlty / nbrsPlus;
   document.getElementById("nbrsOutRes").innerHTML = corrwbs.toFixed(2);
 }
-
-
+function inr() {
+  let ptTime = document.getElementById("floatingInputGroup11").value;
+  let isi = document.getElementById("floatingInputGroup12").value;
+  let conTime = document.getElementById("floatingInputGroup13").value;
+  let intptTime = parseFloat(ptTime);
+  let intIsi = parseFloat(isi);
+  let intConTime = parseFloat(conTime);
+  let intptTimeOverconTime = intptTime / intConTime;
+  let percnt = (intConTime / intptTime) * 100;
+  let inrResult = intptTimeOverconTime ** intIsi;
+  document.getElementById("inrOutRes").innerHTML = inrResult.toFixed(2);
+  document.getElementById("pcOutRes").innerHTML = percnt.toFixed();
+}
+function ca() {
+  let alb = parseFloat(document.getElementById("floatingInputGroup14").value);
+  let cal = parseFloat(document.getElementById("floatingInputGroup15").value);
+  let albu = parseFloat(4.5 - alb);
+  let ionCa = (cal + albu) / 8;
+  if (ionCa <= 1.05 || ionCa >= 1.3) {
+    document.getElementById("caOutRes").style.color = "red";
+    document.getElementById("caOutRes").style.backgroundColor = "black";
+    // document.getElementById("caOutRes").innerHTML = ionCa.toFixed(2);
+  } else {
+    document.getElementById("caOutRes").style.color = "yellow";
+    document.getElementById("caOutRes").style.backgroundColor = "green";
+  }
+  document.getElementById("caOutRes").innerHTML = ionCa.toFixed(2);
+}
+function fena() {
+  let naUrine = parseFloat(document.getElementById("floatingInputGroup16").value);
+  let naSerum = parseFloat(document.getElementById("floatingInputGroup17").value);
+  let creatUrine = parseFloat(document.getElementById("floatingInputGroup18").value);
+  let creatSerum = parseFloat(document.getElementById("floatingInputGroup19").value);
+  let na = naUrine / naSerum;
+  let creat = creatUrine / creatSerum;
+  let fena = (na / creat) * 100;
+  document.getElementById("fenaOutRes").innerHTML = fena.toFixed(1);
+}
 
 let testListTitle = document.querySelectorAll(".list-group a");
 let testListArr = Array.from(testListTitle);
@@ -64,9 +100,27 @@ testListArr.forEach((ele) => {
     });
     e.currentTarget.classList.add("active");
     testNameArr.forEach((el) => {
-      el.style.display = 'none';
+      el.style.display = "none";
     });
-    document.querySelector(e.currentTarget.dataset.test).style.display = 'block';
+    document.querySelector(e.currentTarget.dataset.test).style.display =
+      "block";
   });
 });
 
+// let testListTitle = document.querySelectorAll(".list-group a");
+// let testListArr = Array.from(testListTitle);
+// let testName = document.querySelectorAll(".main .show");
+// let testNameArr = Array.from(testName);
+// testListArr.forEach((ele) => {
+//   ele.addEventListener("click", function (e) {
+//     testListArr.forEach((ele) => {
+//       ele.classList.remove("active");
+//     });
+//     e.currentTarget.classList.add("active");
+//     testNameArr.forEach((el) => {
+//       el.style.display = 'none';
+//     });
+//     document.querySelector(e.currentTarget.dataset.test).style.display = 'block';
+//     // document.querySelector(e.currentTarget.dataset.test). = 'block';
+//   });
+// });
